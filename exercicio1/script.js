6953726e0a8ff46c91e8924aacf0c751e081b16a
vitorramos <ramos6382@gmail.com>
2022-09-08 17:56:19 -0300

    atividade 13

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,2 +1,7 @@
-const num = //seu numero aqui
+const num = prompt("Digite um numero!")
 
+if((num%2)===0||(num%3)===0){
+    console.log("O numero é divisivel por 2 ou por 3!")
+}else{
+    console.log("Não é divisivel nem por 2 nem por 3!")
+}
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,13 +1,17 @@
-//código a ser reescrito
+let nome = prompt("Qual seu nome?").toLowerCase
+let idade = Number(prompt("Qual sua idade?"))
 
-if(nome === "José"){
-	console.log("Oi, Zé!");
-} else {
-	console.log("Olá, "+nome);
-}
+// if(nome === "José"){
+// 	console.log("Oi, Zé!");
+// } else {
+// 	console.log("Olá, "+nome);
+// }
 
-if(idade >= 18){
-	console.log("pode tirar carteira de motorista!");
-} else {
-	console.log("Ainda não pode tirar carteira de motorista");
-}
+// if(idade >= 18){
+// 	console.log("pode tirar carteira de motorista!");
+// } else {
+// 	console.log("Ainda não pode tirar carteira de motorista");
+// }
+
+nome === "josé" ? console.log("Oi, Zé!"):console.log("Olá, "+nome);
+idade >= 18 ? console.log("pode tirar carteira de motorista!") : console.log("Ainda não pode tirar carteira de motorista")
